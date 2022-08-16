d3a10dee97a3a83c52055d676da451ef2ed66edd
Mahmoud Awadeen <mahmoud.awadeen@gmail.com>
2022-08-16 18:16:36 +0200

    Return a json object instead of array

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -1,11 +1,11 @@
 class Api::NewsController < ApplicationController
 
   def get_by_city
-    render json: [
+    render json: {
       city: city,
       latest_news: HeadlinesService.call(city),
       weather: WeatherService.call(city)
-    ]
+    }
   end
 
   private
