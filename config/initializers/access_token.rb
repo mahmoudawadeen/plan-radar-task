eaa3bea5f2b0a176ae1af7be873dfd69a4add7a7
Mahmoud Awadeen <mahmoud.awadeen@gmail.com>
2022-08-16 21:27:23 +0200

    Cache application token generation at startup

--- config/initializers/access_token.rb
+++ config/initializers/access_token.rb
@@ -1,7 +1,10 @@
 Rails.configuration.after_initialize do
   begin
-    Rails.logger.info("Api Access Token: #{Api::AccessToken.first_or_create.token}")
-  rescue ActiveRecord::StatementInvalid => exception
-    Rails.logger.error('Restart application to generate an access token after running the migrations')
+    token = Rails.cache.fetch('access_token') do
+      Api::AccessToken.first_or_create.token
+    end
+    Rails.logger.info("Api Access Token: #{token}")
+  rescue ActiveRecord::ActiveRecordError
+    Rails.logger.error("Restart application to generate an access token after running the migrations")
   end
 end
