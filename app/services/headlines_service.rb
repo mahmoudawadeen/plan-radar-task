4b4c90a824a7f71e9ad8ebd624519ece6b67045c
Mahmoud Awadeen <mahmoud.awadeen@gmail.com>
2022-08-16 21:27:10 +0200

    Cache weather and headline services

--- app/services/headlines_service.rb
+++ app/services/headlines_service.rb
@@ -1,6 +1,8 @@
 class HeadlinesService < ApplicationService
   def call
-    news_api = News.new(ENV.fetch('NEWS_API_KEY','NEWS_API_KEY NOT FOUND'))
-    news_api.get_top_headlines(q: @query)
+    Rails.cache.fetch("headline:#{@query}", expires_in: ENV.fetch("NEWS_API_CACHE_MINUTES", 30).minutes) do
+      news_api = News.new(ENV.fetch('NEWS_API_KEY', 'NEWS_API_KEY NOT FOUND'))
+      news_api.get_top_headlines(q: @query)
+    end
   end
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,15 +1,17 @@
 class WeatherService < ApplicationService
   BASE_URL = 'https://api.openweathermap.org/data/2.5'
-  WEATHER_PATH = "weather?q=%{query}&appid=#{ENV.fetch('WEATHER_API_KEY','WEATHER_API_KEY NOT FOUND')}"
+  WEATHER_PATH = "weather?q=%{query}&appid=#{ENV.fetch('WEATHER_API_KEY', 'WEATHER_API_KEY NOT FOUND')}"
 
   def call
-    weather_path_with_city = WEATHER_PATH % { query: @query }
-    weather = JSON.parse(Net::HTTP.get(URI.parse("#{BASE_URL}/#{weather_path_with_city}")))
-                  .fetch('weather', nil)
-    if weather.nil?
-      raise ActionController::BadRequest.new('Invalid Query')
-    end
+    Rails.cache.fetch("weather:#{@query}", expires_in: ENV.fetch("WEATHER_API_CACHE_MINUTES", 30).minutes) do
+      weather_path_with_city = WEATHER_PATH % { query: @query }
+      weather = JSON.parse(Net::HTTP.get(URI.parse("#{BASE_URL}/#{weather_path_with_city}")))
+                    .fetch('weather', nil)
+      if weather.nil?
+        raise ActionController::BadRequest.new('Invalid Query')
+      end
 
-    weather
+      weather
+    end
   end
 end
