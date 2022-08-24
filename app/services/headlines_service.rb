b77284b30e46908019d8fd5749fc0141ab3d35d5
Mahmoud Awadeen <mahmoud.awadeen@gmail.com>
2022-08-24 13:44:11 +0200

    Fix cache expiration being string

--- app/services/headlines_service.rb
+++ app/services/headlines_service.rb
@@ -1,6 +1,6 @@
 class HeadlinesService < ApplicationService
   def call
-    Rails.cache.fetch("headline:#{@query}", expires_in: ENV.fetch("NEWS_API_CACHE_MINUTES", 30).minutes) do
+    Rails.cache.fetch("headline:#{@query}", expires_in: ENV.fetch("NEWS_API_CACHE_MINUTES", 30).to_i.minutes) do
       news_api = News.new(ENV.fetch('NEWS_API_KEY', 'NEWS_API_KEY NOT FOUND'))
       news_api.get_top_headlines(q: @query)
     end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -3,7 +3,7 @@ class WeatherService < ApplicationService
   WEATHER_PATH = "weather?q=%{query}&appid=#{ENV.fetch('WEATHER_API_KEY', 'WEATHER_API_KEY NOT FOUND')}"
 
   def call
-    Rails.cache.fetch("weather:#{@query}", expires_in: ENV.fetch("WEATHER_API_CACHE_MINUTES", 30).minutes) do
+    Rails.cache.fetch("weather:#{@query}", expires_in: ENV.fetch("WEATHER_API_CACHE_MINUTES", 30).to_i.minutes) do
       weather_path_with_city = WEATHER_PATH % { query: @query }
       weather = JSON.parse(Net::HTTP.get(URI.parse("#{BASE_URL}/#{weather_path_with_city}")))
                     .fetch('weather', nil)
